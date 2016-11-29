5369fc4de8294a71228539ef7c99752142288f87
mourinf <florian.mourin@phelma.grenoble-inp.fr>
2016-11-29 20:16:14 +0100

    serveur acquittement

---
+++ UDP2/src/UDP/Application.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package UDP;
+
+import java.net.DatagramPacket;
+
+/**
+ *
+ * @author Florian
+ */
+public class Application {
+
+    ServeurUdp serveur;
+    int nbTraitement=1;
+
+    public Application(ServeurUdp serveur) {
+        this.serveur = serveur;
+    }
+    
+    
+    public void traiter(DataUdp data){
+        System.out.println("Le "+ data.toString()+" a bien été délivré. Nb messages traités: " + nbTraitement);
+        nbTraitement++;
+    }
+    
+}
